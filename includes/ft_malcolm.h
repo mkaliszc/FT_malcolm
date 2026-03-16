caea09bb1aa77fbefa75ebd3f9be0342cee78b29
mkaliszc <milan.kaliszczak@gmail.com.com>
2026-03-16 15:05:19 +0100

    + | includes for authorized function

---
+++ includes/ft_malcolm.h
@@ -0,0 +1,23 @@
+#ifndef FT_MALCOLM
+# define FT_MALCOLM
+
+# include "libft.h"
+// * included by libft.h
+// stdlib
+// stdio
+// ctype
+// unistd
+// stdarg
+// fcntl
+// stdbool
+
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <signal.h>
+# include <netdb.h>
+# include <ifaddrs.h>
+# include <string.h>
+
+#endif

21c9a77dcb34cd2a008ff269e7a8dda4d26e1670
mkaliszc <milan.kaliszczak@gmail.com.com>
2026-03-19 16:43:34 +0100

    + | todo main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 # include "ft_malcolm.h"
 
+/* TODO: 
+	- Check parameters to be sure (order, host, etc)
+	- Final goal is to wait for an arp request from the target and
+		give an arp reply to poison the arp cache of the target.
+*/
+
 int main(int argc, char **argv) {
 	
 }
